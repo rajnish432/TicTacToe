4b7e1dd23326c3629657f3eabd98138f7d96849c
rajnish432 <rajnish.kahar1996@gmail.com>
2020-02-05 17:19:26 +0530

    ADDED: Selection of Valid Empty cell

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -4,6 +4,18 @@ echo "WELCOME TO TIC_TAC_TOE SIMULATOR";
 declare -A board
 ROWS=3;
 COLUMNS=3;
+totalMove=0;
+
+function initializeBoard()
+{
+   for (( i=0 ; i<ROWS; i++ ))
+   do
+      for (( j=0 ; j<COLUMNS; j++ ))
+      do
+         board[$i,$j]="+";
+      done
+   done
+}
 
 function gameBoard()
 {
@@ -12,7 +24,6 @@ function gameBoard()
 		echo "---------------"
 			for (( j=0; j<COLUMNS; j++))
 			do
-				board[$i,$j]="+"
 				echo -n "| ${board[$i,$j]} |"
 			done
 		echo
@@ -36,5 +47,25 @@ function playerLetterAssignment()
 	fi
 }
 
+function gamePlay()
+{
+	playerLetterAssignment
+	while [[ $totalMove -lt 9 ]]
+	do
+			read -p "Enter the row: " row
+			read -p "Enter the Col: " col
+				if [[ ${board[$row,$col]} == + ]]
+				then
+					board[$row,$col]=$playerLetter
+					gameBoard;
+					((totalMove++))
+				else
+					echo "Invalid move"
+				fi
+	done
+}
+
+initializeBoard
 gameBoard
-playerLetterAssignment
+gamePlay
+

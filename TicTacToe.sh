e97d109e9c3aa9437055817e5beda1d77b1b9b03
rajnish432 <rajnish.kahar1996@gmail.com>
2020-02-05 16:36:16 +0530

    ADDED: Game Board Initialization

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,3 +1,22 @@
 #! /bin/bash
 
 echo "WELCOME TO TIC_TAC_TOE SIMULATOR";
+declare -A board
+ROWS=3;
+COLUMNS=3;
+
+function gameBoard()
+{
+	for (( i=0; i<ROWS; i++ ))
+	do
+		echo "---------------"
+			for (( j=0; j<COLUMNS; j++))
+			do
+				board[$i,$j]="+"
+				echo -n "| ${board[$i,$j]} |"
+			done
+		echo
+	done
+	echo "---------------"
+}
+gameBoard

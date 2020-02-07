2ffcdd43af2fd750f9aa57c3965597f5a4a63a01
rajnish432 <rajnish.kahar1996@gmail.com>
2020-02-07 11:40:10 +0530

    ADDED: Computer Play with Tie and Switch

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -6,6 +6,7 @@ ROWS=3;
 COLUMNS=3;
 totalMove=0;
 winner=false;
+flag=0;
 
 function initializeBoard()
 {
@@ -37,13 +38,17 @@ function playerLetterAssignment()
 	if [[ $((RANDOM%2)) -eq 0 ]]
 	then
 		playerLetter=X
+		computerLetter=0
 	else
 		playerLetter=0;
+		computerLetter=X;
 	fi
 	if [[ $playerLetter == X ]]
 	then
+		flag=0;
 		echo "Player plays first"
 	else
+		flag=1;
 		echo "computer Plays first"
 	fi
 }
@@ -54,7 +59,7 @@ function winningRow()
 	do
 		for ((j=0; j<COLUMNS; j++))
 		do
-		if [[ ${board[$i,$j]} == ${board[$i,$((j+1))]} && ${board[$i,$((j+1))]} == ${board[$i,$((j+2))]} && ${board[$i,$j]} == $playerLetter ]]
+		if [[ ${board[$i,$j]} == ${board[$i,$((j+1))]} && ${board[$i,$((j+1))]} == ${board[$i,$((j+2))]} && ${board[$i,$j]} == $1 ]]
 		then
 			winner=true;
 		fi
@@ -68,7 +73,7 @@ function winningColumn()
    do
       for ((j=0; j<COLUMNS; j++))
       do
-      if [[ ${board[$i,$j]} == ${board[$((i+1)),$j]} && ${board[$((i+1)),$j]} == ${board[$((i+2)),$j]} && ${board[$i,$j]} == $playerLetter ]]
+      if [[ ${board[$i,$j]} == ${board[$((i+1)),$j]} && ${board[$((i+1)),$j]} == ${board[$((i+2)),$j]} && ${board[$i,$j]} == $1 ]]
       then
          winner=true;
       fi
@@ -90,26 +95,53 @@ function gamePlay()
 	playerLetterAssignment
 	while [[ $totalMove -lt 9 ]]
 	do
+		if [[ $flag -eq 0 ]]
+		then
 			read -p "Enter the row: " row
 			read -p "Enter the Col: " col
 				if [[ ${board[$row,$col]} == + ]]
 				then
 					board[$row,$col]=$playerLetter
 					gameBoard;
-					winningRow
-					winningColumn
+					winningRow	$playerLetter
+					winningColumn	$playerLetter
 					winningDiagonal 0 0 1 1 2 2
 					winningDiagonal 0 2 1 1 2 0
-						if [[ $winner == true ]]
-						then
-							echo "Winner";
-							exit;
-						fi
+					getWinner "Player"
+					flag=1
 					((totalMove++))
-				else
-					echo "Invalid move"
 				fi
+		elif [[ $flag -eq 1 ]]
+		then
+			row=$((RANDOM%3))
+			col=$((RANDOM%3))
+				if [[ ${board[$row,$col]} == + ]]
+      		then
+					board[$row,$col]=$computerLetter
+					gameBoard;
+					winningRow $computerLetter
+					winningColumn $computerLetter
+					winningDiagonal 0 0 1 1 2 2
+					winningDiagonal 0 2 1 1 2 0
+					getWinner "computer"
+					flag=0;
+					((totalMove++))
+				fi
+		fi
 	done
+	if [[ $totalMove -eq 9 && $winner == false ]]
+	then
+		echo " Game Draw"
+	fi
+}
+
+function getWinner()
+{
+	if [[ $winner == true ]]
+	then
+		echo "Winner is $1";
+		exit;
+	fi
 }
 
 initializeBoard
